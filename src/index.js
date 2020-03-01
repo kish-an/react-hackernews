b8e2e3d879e15186f3479137518ee4c1329e2cc6
Kishan <kishan.o@hotmail.com>
2020-03-01 04:16:53 +0000

    Setup

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+    return (
+        <div>
+            Hello, world!
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const copyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js',
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            { test: /\.(js)$/, use: 'babel-loader' },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+        ],
+    },
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new copyWebpackPlugin([
+            { from: '_redirects' }
+        ])
+    ],
+    devServer: {
+        historyApiFallback: true,
+    },
+}
