81187ee3724dcd9634f8af3ec664989456ef56d1
Kishan <kishan.o@hotmail.com>
2020-03-02 02:17:17 +0000

    Add global theme with context

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import ThemeContext from '../contexts/theme';
 
 const styles = {
     color: '#d35400',
 }
 
-const Nav = () => {
+const Nav = ({ toggleTheme }) => {
+    const theme = useContext(ThemeContext);
+
     return (
         <nav className='row space-between'>
             <ul className='row nav'>
@@ -20,8 +24,19 @@ const Nav = () => {
                     </NavLink>
                 </li>
             </ul>
+            <button
+                className='btn-clear'
+                onClick={toggleTheme}
+                style={{fontSize: 30}}
+            >
+                {theme === 'light' ? '🔦' : '💡'}
+            </button>
         </nav>
     )
+}
+
+Nav.propTypes = {
+    toggleTheme: PropTypes.func.isRequired,
 }
 
 export default Nav;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Stories from './components/Stories';
 import Nav from './components/Nav';
+import { ThemeProvider } from './contexts/theme';
 
 const App = () => {
+    const [theme, setTheme] = useState('light');
+
+    const toggleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+        console.log(theme);
+    };
+
     return (
         <Router>
-            <div className="container">
-                <Nav />
+            <ThemeProvider value={theme}>
+                <div className={theme}>
+                    <div className="container">
+                        <Nav toggleTheme={toggleTheme} />
 
-                <Switch>
-                    <Route
-                        exact
-                        path='/'
-                        render={props => <Stories {...props} type='top' />}
-                    />
-                    <Route
-                        path='/new'
-                        render={props => <Stories {...props} type='new' />}
-                    />
-                    <Route render={() => <h1 style={{ textAlign: 'center', color: '#c0392b' }}>🚨 404: Page not found!</h1>} />
-                </Switch>
-            </div>
+                        <Switch>
+                            <Route
+                                exact
+                                path='/'
+                                render={props => <Stories {...props} type='top' />}
+                            />
+                            <Route
+                                path='/new'
+                                render={props => <Stories {...props} type='new' />}
+                            />
+                            <Route render={() => <h1 style={{ textAlign: 'center', color: '#c0392b' }}>🚨 404: Page not found!</h1>} />
+                        </Switch>
+                    </div>
+                </div>
+            </ThemeProvider>
         </Router>
     )
 }
